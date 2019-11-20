2349b58ec9c35cebb49d642fb443def5ed96519d
Kyle Boehlen <kyleboehlen@gmail.com>
2019-11-19 18:26:08 -0700

    Splash page template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,44 @@
 @extends('layouts.base')
 
 @section('body')
-    Paint and Noise shit goes here
+    <body>
+        {{-- Main logo --}}
+        <img src="{{ route('logo') }}" id="main-logo" />
+
+        {{-- Title --}}
+        <h1>{{ env('APP_NAME', 'P&N') }}</h1>
+
+        {{-- Social section --}}
+        <h2>Follow Us</h2>
+
+        <div class="figure-container">
+            {{-- Iterate through social class accounts property --}}
+            @foreach(App\Http\Helpers\Social::ACCOUNTS as $account)
+                <figure>
+                    <a href="{{ $account['url'] . $account['username'] }}">
+                        <img src="{{ route('social', ['icon' => $account['icon']]) }}"/>
+                        <figcaption>{{ $account['service'] }}</figcaption>
+                    </a>
+                </figure>
+            @endforeach
+        </div>
+
+        {{-- Team section --}}
+        <h2>Our Team</h2>
+
+        <div class="figure-container">
+            {{-- Iterate through team class members property --}}
+            @foreach(App\Http\Helpers\Team::MEMBERS as $team_member)
+                <figure>
+                    <img src="{{ route('team', ['name' => $team_member['name']]) }}"/>
+                    <figcaption>{{ $team_member['full_name'] }}</figcaption>
+                </figure>
+            @endforeach
+        </div>
+
+        <footer>
+            {{-- copyright --}}
+            <p class="copyright">&copy; {{ env('APP_NAME', 'P&N') }}</p>
+        </footer>
+    </body>
 @endsection
