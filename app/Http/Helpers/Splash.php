6d122c640afecabffb42fca13412e9e12fced82e
Kyle Boehlen <kboehlen@kengarff.com>
2019-11-20 17:11:23 -0700

    Typo in splash description

--- app/Http/Helpers/Splash.php
+++ app/Http/Helpers/Splash.php
@@ -6,6 +6,6 @@ class Splash
 {
     const DESCRIPTION = "In the noble pursuit of exceptional art, music, and creative flow.
     Paint & Noise aims to explore talented individuals and feature them on a platform for the world to see.
-    By being a community of like minded people and inspired creators we want to engage in a conversation with each other and give a sold foundation for artists to present their finished projects to other artists and with the audience as a whole.
+    By being a community of like minded people and inspired creators we want to engage in a conversation with each other and give a solid foundation for artists to present their finished projects to other artists and with the audience as a whole.
     Our goal is to inspire collaborations, creative energy, and show support for our fellow hoomans.";
 }
